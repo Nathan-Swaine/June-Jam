27279e13b7dfbef182fa5218c24e9000e2aed708
Nathan-Swaine <nathanswaine18@gmail.com>
2021-06-14 14:45:05 +0100

    tidyied movement

---
+++ New Unity Project/Assets/Scenes/Scripts/move.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{   
+    float gravity = -30f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; 
+    public LayerMask groundMask; 
+    public bool isGrounded;
+    public Rigidbody me;  
+    public Rigidbody selectedWeapon;
+    public float xDiretionMovement;
+   
+
+    void Start()
+    {
+        me = GetComponent<Rigidbody>(); // get the ridigid body from the inspector.
+       
+    }
+
+    void Update() // Update is called once per frame
+    {
+
+        xDiretionMovement = Input.GetAxis("Horizontal") * 50f;  // move the player left or right, if they are not moving then its 50 * 0, so they wont move
+        me.AddForce(xDiretionMovement, 0, 0, ForceMode.Force);  //these two lines handle horizontal movement across the screen.
+        selectedWeapon.AddForce(xDiretionMovement,0,0,ForceMode.Force);
+        checkGround(groundCheck, 0.4f, groundMask); //are we in air?
+        checkGravity(); //check if we are jumping / on ground
+      
+    }
+
+    void checkGravity()
+    {
+    
+        if(!isGrounded)//in air
+        {
+            gravity -= 8*Time.deltaTime;
+            me.AddForce(xDiretionMovement, gravity, 0, ForceMode.Acceleration);
+            selectedWeapon.AddForce(xDiretionMovement,gravity,0,ForceMode.Acceleration);
+
+        }
+        else// on ground
+        {
+            gravity = -1f;// sometimes the checkground will return true despite been slightly above the ground, this will 'suck' the player down slightly. 
+        }
+        if (Input.GetKeyDown("space") && isGrounded) //on ground and pressing space
+            {
+                me.AddForce(xDiretionMovement, 200f,0, ForceMode.Impulse);
+                selectedWeapon.AddForce(xDiretionMovement,200f,0,ForceMode.Impulse);
+
+            }
+    }       
+
+    void checkGround(Transform Object, float Radius , LayerMask Layer)
+    {
+        isGrounded = Physics.CheckSphere(Object.position, Radius, Layer); //returns a bool if a sphere around object, with the radius 'radius' is touching anything with the 'layer'
+        checkGravity();
+    }
+}
--- New Unity Project/Assets/Scenes/Scripts/weapon.cs
+++ New Unity Project/Assets/Scenes/Scripts/weapon.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class weapon : MonoBehaviour
 {
-
-    public Rigidbody rb; 
+    float gravity = -30f;
+    
+    public Rigidbody rb;
     public Transform Object;
-
-    // Start is called before the first frame update
+    public LayerMask groundMask; 
+ 
     void Start()
     {
-       rb = GetComponent<Rigidbody>(); 
-       Object = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         if(Input.GetButtonDown("Fire1"))
         {
             shoot();
         }
     }
-
     void shoot()
     {
+        
         Object.localRotation = Quaternion.Euler(0,0,45);
-        rb.AddForce(45f,0,0, ForceMode.Acceleration);
+        rb.AddForce(45f,10,0, ForceMode.Acceleration);
     }
 }
