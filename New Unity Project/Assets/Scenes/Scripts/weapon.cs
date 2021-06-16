60449d3fec040b5f2eac9deda2b0d285e2f3e1fc
Nathan-Swaine <nathanswaine18@gmail.com>
2021-06-16 16:06:23 +0100

    Throwing weapons using physics

--- New Unity Project/Assets/Scenes/Scripts/weapon.cs
+++ New Unity Project/Assets/Scenes/Scripts/weapon.cs
@@ -7,60 +7,32 @@ public class weapon : MonoBehaviour
     public Rigidbody rb;
     public Transform spearBottom;
     public Transform spearTip;
-    public controller controller;
     public LayerMask groundMask;
     public Transform Spear;
-    public bool isGrounded;
-    public bool isHolding;
-    public float force = 30f;
-    public float gravity = -30f; 
+    public bool isHolding;   
+    public BoxCollider spearCollider;
     void Start()
     {
-        
+        spearCollider.GetComponent<Collider>().enabled = false;
         rb = GetComponent<Rigidbody>(); 
+        rb.isKinematic = true;
+        rb.useGravity = false;
         isHolding = true;
     }
-
-    // Update is called once per frame
-    void Update()
+    void Update() // Update is called once per frame
     {   
       if (Input.GetButtonDown("Fire1"))
         {
-            fire(rb, force);
+            fire(rb, 500);
         }    
     }
-
-    void checkGravity(Rigidbody Object, float gravity)
-    {
-        if(!isGrounded)//in air
-        {
-            gravity -= 8*Time.deltaTime;
-            Object.AddForce(0, gravity, 0, ForceMode.Acceleration); //selectedWeapon.AddForce(xDiretionMovement,gravity,0,ForceMode.Acceleration);
-        }
-        else// on ground
-        {
-            gravity = -1f;// sometimes the checkground will return true despite been slightly above the ground, this will 'suck' the player down slightly. 
-        }
-        if (Input.GetKeyDown("space") && isGrounded) //on ground and pressing space
-            {
-                Object.AddForce(0, 300f, 0, ForceMode.Impulse);
-                Object.AddForce(0, 0, 0, ForceMode.Force);//selectedWeapon.AddForce(xDiretionMovement,200f,0,ForceMode.Impulse);
-            }
-    }       
-    void checkGround(Transform Object, float Radius , LayerMask Layer)
-    {
-        isGrounded = Physics.CheckSphere(Object.position, Radius, Layer); //returns a bool if a sphere around object, with the radius 'radius' is touching anything with the 'layer'
-    }
-
     void fire(Rigidbody Object, float force)
     {
-        checkGround(spearTip, 3f, groundMask);
-        checkGravity(rb , gravity);
-        fire(rb, force);
-        isHolding = false;
-        rb.isKinematic = false;
+        spearCollider.GetComponent<Collider>().enabled = true;
         Spear.transform.parent = null;
-        Object.AddForce(transform.forward *force, ForceMode.Force);
+        Object.isKinematic = false;
+        Object.useGravity = true;
+        Object.AddForce(100f,force,0, ForceMode.Force);
+        isHolding = false;        
     }
-    
 }
