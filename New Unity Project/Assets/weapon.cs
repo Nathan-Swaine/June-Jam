621147c657f85aa4b665a20fb21669313fe76988
Nathan-Swaine <nathanswaine18@gmail.com>
2021-06-18 16:33:03 +0100

    refactoring and tweaks

--- New Unity Project/Assets/weapon.cs
+++ New Unity Project/Assets/weapon.cs
@@ -6,10 +6,11 @@ public class weapon : MonoBehaviour{
     public Rigidbody rb;
     public Transform weaponCurvePoint, hand;
     public GameObject currentWeapon;
-    public bool isHolding= true , inAir = false, isReturning= false;   
     public BoxCollider tipCollider;
-    public float throwPower= 100f, throwDirection, time = 0.0f; 
+    public float throwPower= 100f, throwDirection, time = 0.0f, lastFacing; 
     public Vector3 oldPos, newPos;
+    public enum weaponState{holding, thrown, flying, returning};
+    public weaponState weaponstate;
 
 
     void Start()
@@ -17,24 +18,24 @@ public class weapon : MonoBehaviour{
             rb = GetComponent<Rigidbody>(); 
             rb.isKinematic = true;
             rb.useGravity = false;
-            isHolding = true;
-        }
+            weaponstate = weaponState.holding;
+            }
     void Update() // Update is called once per frame
         {   
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButtonDown("Fire1"))//a    && weaponstate == weaponState.holding)
                 {
                     fire(rb, throwPower, currentWeapon.transform);
                 } 
-            if (inAir)
+            if (weaponstate == weaponState.flying)
                 {
                     tipCollider.GetComponent<Collider>().enabled = true;
                 }
            
-            if (!isHolding && !inAir && Input.GetButtonDown("Fire2"))
+            if (weaponstate == weaponState.thrown && Input.GetButtonDown("Fire2"))
             {
-                isReturning = true;
+                weaponstate = weaponState.returning;
             }
-            if (isReturning)
+            if (weaponstate == weaponState.returning)
             {
                 recall(rb, weaponCurvePoint, hand);
             }
@@ -50,45 +51,44 @@ public class weapon : MonoBehaviour{
                     time += Time.deltaTime;
                     Vector3 newPos= Vector3.Lerp(oldPos, hand.position, time);
                     weapon.position = newPos;
-                    
-                    
-                    
-                    
-
+                    weaponstate = weaponState.holding;
                 }
             else if( time >= 1.00f)
             {
-                isReturning = false;
+                weaponstate = weaponState.holding;
             }
         }
     void fire(Rigidbody Object, float force, Transform weapon)
         {
-            if (isHolding)
-            {
-                throwDirection = Input.GetAxis("Horizontal");
-                    
-                if (throwDirection > 0 && throwDirection < 1) //this if statement and the one above are so that we can still throw the weapon in the direction the player is facing without diluting the throw power because of fractional scaling.
-                    {
-                        throwDirection = 1;
-                    } 
-                else if(throwDirection < 0 && throwDirection > -1) 
-                    {
-                        throwDirection =-1;
-                    }
-                weapon.transform.parent = null; //de-couple from the parent, so we can fly off into the distance 
-                Object.isKinematic = false; // object is not static
-                Object.useGravity = true; // object is subjected to gravity
-                Object.AddForce(force *throwDirection, force/2, 0, ForceMode.Impulse); //Impluse looks better than acceleration or Force
-                
-                isHolding = false;//we are not holding the weapon
-                inAir = true; // because its in the air
-                weaponSpin(weapon); // needs to spin to look good
-            }
+         
+            weapon.transform.parent = null; //de-couple from the parent, so we can fly off into the distance 
+            Object.isKinematic = false; // object is not static
+            Object.useGravity = true; // object is subjected to gravity
+            getThrowDirection();
+            Object.AddForce(force* throwDirection, force/2, 0, ForceMode.Impulse); //Impluse looks better than acceleration or Force
+            
+            weaponstate = weaponState.flying;
+            weaponSpin(weapon); // needs to spin to look good
+        
         }
 
+    void getThrowDirection()
+        {
+            throwDirection = Input.GetAxis("Horizontal");
+            if (throwDirection > 0 && throwDirection < 1) //this if statement and the one above are so that we can still throw the weapon in the direction the player is facing without diluting the throw power because of fractional scaling.
+                {
+                    throwDirection = 1;
+                    lastFacing = throwDirection;
+                } 
+            else if(throwDirection < 0 && throwDirection > -1) 
+                {
+                    throwDirection =-1;
+                    lastFacing = throwDirection;
+                }
+        }
     void weaponSpin(Transform weapon)
         {
-            if(!isHolding && inAir) // if traveling 
+            if(weaponstate == weaponState.flying) // if traveling 
                 {
                     weapon.transform.RotateAround(weapon.transform.position, Vector3.back, 2000* Time.deltaTime);
                 }
@@ -102,7 +102,7 @@ public class weapon : MonoBehaviour{
                 }
             else
                 {                    
-                    inAir = false;
+                    weaponstate = weaponState.thrown;
                 }
             
             
