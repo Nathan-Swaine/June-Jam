fa5dda9802f9ae656835bf2adaf955cdaa12aa0b
Nathan-Swaine <nathanswaine18@gmail.com>
2021-06-27 22:53:26 +0100

    tweaks

--- New Unity Project/Assets/player assets/scripts/spawn.cs
+++ New Unity Project/Assets/player assets/scripts/spawn.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public Transform[] spawnPoints;
+    public GameObject spearPreFab; 
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,23 @@ public class spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float timeBetween = 100f; 
+        float spawnTime = 2f; 
+        if (Time.time >= spawnTime)
+        {
+            spawnSpear();
+            spawnTime += Time.time + timeBetween;
+        }
+        spawnSpear();
+    }
+
+    void spawnSpear()
+    {
+        int randInt = Random.Range(0 , spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(spearPreFab, spawnPoints[i].position, Quaternion.identity);
+        }
     }
 }
